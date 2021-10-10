b4a758b45afc454501fa5d6dc24e882716ae37ec
Behrooze Sirang <behrooze@gmail.com>
2021-10-10 13:47:44 -0700

    update service server example

---
+++ trellis/examples/service_server/app.hpp
@@ -0,0 +1,23 @@
+#ifndef TRELLIS_EXAMPLES_SERVICE_SERVER_APP_HPP
+#define TRELLIS_EXAMPLES_SERVICE_SERVER_APP_HPP
+
+#include "trellis/core/node.hpp"
+#include "trellis/examples/proto/addition_service.pb.h"
+
+namespace trellis {
+namespace examples {
+namespace service_server {
+
+class App {
+ public:
+  App(const trellis::core::Node& node, const trellis::core::Config& config);
+
+ private:
+  trellis::core::ServiceServer<trellis::examples::proto::AdditionService> server_;
+};
+
+}  // namespace service_server
+}  // namespace examples
+}  // namespace trellis
+
+#endif  // TRELLIS_EXAMPLES_SERVICE_SERVER_APP_HPP
