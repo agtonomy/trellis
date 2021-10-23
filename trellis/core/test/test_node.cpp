4e5704fd7745b0378efed1645cc5a9fc9a400ddb
Behrooze Sirang <behrooze@gmail.com>
2021-10-22 17:06:09 -0700

    Node runner thread test

---
+++ trellis/core/test/test_node.cpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2021 Agtonomy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <gtest/gtest.h>
+
+#include "trellis/core/test/test_fixture.hpp"
+
+using namespace trellis::core;
+using namespace trellis::core::test;
+
+TEST_F(TrellisFixture, StartAndStopNode) {
+  static unsigned receive_count{0};
+
+  // Simply start the runner thread and then test that it will gracefully
+  // stop without hanging
+  StartRunnerThread();
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+  Stop();
+}
