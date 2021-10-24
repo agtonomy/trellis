ba26fde3a98c4ca3724cd99e346c600d0e0296c4
Behrooze Sirang <behrooze@gmail.com>
2021-10-23 17:40:20 -0700

    watchdog test

--- trellis/core/test/test_message_consumer.cpp
+++ trellis/core/test/test_message_consumer.cpp
@@ -63,3 +63,57 @@ TEST_F(TrellisFixture, MultipleMessageTypesWithIndividualCallbacks) {
   ASSERT_EQ(receive_count_1, 10U);
   ASSERT_EQ(receive_count_2, 10U);
 }
+
+TEST_F(TrellisFixture, MultipleMessageTypesWithIndividualCallbacksAndWatchdogs) {
+  static unsigned receive_count_1{0};
+  static unsigned receive_count_2{0};
+  static unsigned watchdog_count_1{0};
+  static unsigned watchdog_count_2{0};
+  // Simply start the runner thread and then test that it will gracefully
+  // stop without hanging
+  StartRunnerThread();
+
+  auto pub = node_.CreatePublisher<test::Test>("consumer_topic_1");
+  auto pub2 = node_.CreatePublisher<test::TestTwo>("consumer_topic_2");
+
+  trellis::core::MessageConsumer<1, test::Test, test::TestTwo> inputs_{
+      node_,
+      {{"consumer_topic_1", "consumer_topic_2"}},
+      {[this](const test::Test& msg, const time::TimePoint&) {
+         ASSERT_EQ(receive_count_1, msg.id());
+         ++receive_count_1;
+       },
+       [this](const test::TestTwo& msg, const time::TimePoint&) {
+         ASSERT_FLOAT_EQ(receive_count_2, msg.foo() / 2.0);
+         ++receive_count_2;
+       }},
+      {{50U, 100U}},
+      {{[]() {
+          ++watchdog_count_1;
+        },
+        []() {
+          ++watchdog_count_2;
+        }}}};
+
+  WaitForDiscovery();
+
+  // Publish messages on both topics
+  for (unsigned i = 0; i < 10U; ++i) {
+    test::Test test_msg;
+    test::TestTwo test_msg2;
+    test_msg.set_id(i);
+    test_msg.set_msg("hello world");
+    test_msg2.set_foo(i * 2.0);
+    pub->Send(test_msg);
+    pub2->Send(test_msg2);
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+  }
+
+  std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+  ASSERT_EQ(receive_count_1, 10U);
+  ASSERT_EQ(receive_count_2, 10U);
+
+  ASSERT_EQ(watchdog_count_1, 1U);
+  ASSERT_EQ(watchdog_count_2, 1U);
+}
