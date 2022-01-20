ed6900e2b560b51e4b6e58357402c9177eea6500
Behrooze Sirang <behrooze@gmail.com>
2022-01-19 16:32:04 -0800

    Examples - Add Initial Delay for Publish / Service Call (#43)

--- trellis/examples/publisher/app.cpp
+++ trellis/examples/publisher/app.cpp
@@ -9,15 +9,18 @@ using namespace trellis::core;
 App::App(const Node& node, const Config& config)
     : publisher_{node.CreatePublisher<trellis::examples::proto::HelloWorld>(
           config["examples"]["publisher"]["topic"].as<std::string>())},
-      timer_{node.CreateTimer(config["examples"]["publisher"]["interval_ms"].as<unsigned>(), [this]() { Tick(); })} {}
+      timer_{node.CreateTimer(
+          config["examples"]["publisher"]["interval_ms"].as<unsigned>(), [this]() { Tick(); },
+          config["examples"]["publisher"]["initial_delay_ms"].as<unsigned>())} {}
 
 void App::Tick() {
-  Log::Info("Publishing message number {}", send_count_);
+  const unsigned msg_number = send_count_++;
+  Log::Info("Publishing message number {}", msg_number);
 
   trellis::examples::proto::HelloWorld msg;
   msg.set_name("Publisher Example");
   msg.set_msg("Hello World!");
-  msg.set_id(++send_count_);
+  msg.set_id(msg_number);
   publisher_->Send(msg);
 }
 
