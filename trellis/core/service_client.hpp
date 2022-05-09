f726549d87f274f14e35f1dbc7f42f0e8cb56b13
Behrooze Sirang <behrooze@gmail.com>
2022-05-09 10:46:45 -0700

    type change

--- trellis/core/service_client.hpp
+++ trellis/core/service_client.hpp
@@ -56,7 +56,7 @@ class ServiceClientImpl {
 
     auto loop = ev_loop_;
     client->AddResponseCallback(
-        [timeout_timer, cb, loop, client](const struct eCAL::SServiceInfo& service_info, const std::string& response) {
+        [timeout_timer, cb, loop, client](const struct eCAL::SServiceResponse& service_response, const std::string& response) {
           if (timeout_timer) {
             if (timeout_timer->Expired()) {
               // We've already timed out, bail now even if we have a response
@@ -66,7 +66,7 @@ class ServiceClientImpl {
             timeout_timer->Stop();
           }
           RESP_T resp;
-          const ServiceCallStatus status = (service_info.call_state != call_state_executed) ? kFailure : kSuccess;
+          const ServiceCallStatus status = (service_response.call_state != call_state_executed) ? kFailure : kSuccess;
           if (status == kSuccess) {
             resp.ParseFromString(response);
           }
