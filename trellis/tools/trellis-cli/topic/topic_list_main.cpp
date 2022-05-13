963d0c0d80a25f4e0a8e5c09093a450ddb96ca77
Behrooze Sirang <behrooze@gmail.com>
2022-05-12 17:25:15 -0700

    more implementation

--- trellis/tools/trellis-cli/topic/topic_list_main.cpp
+++ trellis/tools/trellis-cli/topic/topic_list_main.cpp
@@ -18,6 +18,7 @@
 #include <cxxopts.hpp>
 #include <thread>
 
+#include "VariadicTable.h"
 #include "trellis/core/monitor_interface.hpp"
 #include "trellis/tools/trellis-cli/constants.hpp"
 
@@ -25,9 +26,37 @@ namespace trellis {
 namespace tools {
 namespace cli {
 
+struct TopicInfo {
+  unsigned publisher_count{0};
+  unsigned subscriber_count{0};
+  std::unordered_set<std::string> types{};
+  std::unordered_set<std::string> hostnames;
+};
+
+std::string StringifySet(const std::unordered_set<std::string>& set) {
+  std::string result;
+  for (const auto& item : set) {
+    if (result.size() > 0) {
+      result += "," + item;
+    } else {
+      result = item;
+    }
+  }
+  return result;
+}
+
+bool IsRawTopic(const std::string& topic) {
+  // check if name ends with "/raw"
+  if (topic.size() >= 4) {
+    return topic.substr(topic.size() - 4, topic.size()) == "/raw";
+  }
+
+  return false;
+}
+
 int topic_list_main(int argc, char* argv[]) {
   cxxopts::Options options(topic_list_command.data(), topic_list_command_desc.data());
-  options.add_options()("h,help", "print usage");
+  options.add_options()("h,help", "print usage")("r,raw", "raw dump");
 
   auto result = options.parse(argc, argv);
   if (result.count("help")) {
@@ -41,7 +70,38 @@ int topic_list_main(int argc, char* argv[]) {
   std::this_thread::sleep_for(std::chrono::milliseconds(monitor_delay_ms));
 
   trellis::core::MonitorInterface mutil;
-  mutil.PrintTopics();
+
+  if ((result.count("raw"))) {
+    mutil.PrintTopics();
+  } else {
+    const auto& snapshot = mutil.UpdateSnapshot();
+    std::map<std::string, TopicInfo> topic_map;
+
+    for (const auto& topic : snapshot.topics()) {
+      const bool is_publisher = (topic.direction() == "publisher");
+      std::string topic_name = topic.tname();
+      if (IsRawTopic(topic.tname()) && is_publisher) {
+        // We grab the real type from the raw topic
+        const std::string actual_topic_name = topic.tname().substr(0, topic.tname().size() - 4);
+        topic_map[actual_topic_name].types.insert(topic.ttype());
+      } else {
+        TopicInfo& topic_info = topic_map[topic.tname()];
+        if (is_publisher) {
+          ++topic_info.publisher_count;
+          topic_info.hostnames.insert(topic.hname());
+        } else {
+          ++topic_info.subscriber_count;
+        }
+      }
+    }
+
+    VariadicTable<std::string, int, int, std::string, std::string> vt({"Name", "PubCnt", "SubCnt", "Host(s)", "Type"});
+    for (const auto& [topic, info] : topic_map) {
+      vt.addRow(topic, info.publisher_count, info.subscriber_count, StringifySet(info.hostnames),
+                StringifySet(info.types));
+    }
+    vt.print(std::cout);
+  }
 
   eCAL::Finalize();
   return 0;
