ef50dc85d11af5badae9ca1487df100817c2b97f
Premkumar Natarajan <prem.avn@gmail.com>
2022-06-02 13:40:39 -0700

    Install bazelisk for target architecture (#79)
    Install the correct bazelisk binary release for the target architecture.

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,20 @@
 
 set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+ARCHITECTURE="$(uname -m)"
 DOCKER_IMAGE_NAME="trellis-docker"
+
+case $ARCHITECTURE in
+    x86_64)
+        DOCKER_IMAGE_NAME+=":amd64";;
+    arm64)
+        DOCKER_IMAGE_NAME+=":arm64";;
+    aarch64)
+        DOCKER_IMAGE_NAME+=":arm64";;
+    *)
+        echo "Failed to build. Unexpected architecture: $ARCHITECTURE"; exit 1;;
+esac
 
 cd "$SCRIPT_DIR"
 DOCKER_BUILDKIT=1 docker build . -t "$DOCKER_IMAGE_NAME"
--- docker/shell.sh
+++ docker/shell.sh
@@ -5,6 +5,20 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 WORKSPACE_ROOT="$SCRIPT_DIR/.."
 HOME_DIR="/root/trellis"
 
+ARCHITECTURE="$(uname -m)"
+DOCKER_IMAGE_NAME="trellis-docker"
+
+case $ARCHITECTURE in
+    x86_64)
+        DOCKER_IMAGE_NAME+=":amd64";;
+    arm64)
+        DOCKER_IMAGE_NAME+=":arm64";;
+    aarch64)
+        DOCKER_IMAGE_NAME+=":arm64";;
+    *)
+        echo "Unexpected architecture: $ARCHITECTURE"; exit 1;;
+esac
+
 # shellcheck disable=SC2068
 docker run -it --rm \
   --network host \
@@ -12,4 +26,4 @@ docker run -it --rm \
   --pid host \
   -v "$WORKSPACE_ROOT:$HOME_DIR" \
   -v "$HOME/.cache:/root/.cache" \
-  trellis-docker $@
+  $DOCKER_IMAGE_NAME $@
