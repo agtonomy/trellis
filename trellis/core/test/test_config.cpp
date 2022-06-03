de48dd279b8b93447736d159394d8e5a1e8197d9
Behrooze Sirang <behrooze@gmail.com>
2022-06-03 13:33:43 -0700

    finish first test

--- trellis/core/test/test_config.cpp
+++ trellis/core/test/test_config.cpp
@@ -23,12 +23,26 @@ using namespace trellis::core;
 
 TEST(TrellisConfigAPI, OverlaySmokeTest) {
   trellis::core::Config config("trellis/core/test/test_base_config.yml");
+
+  // First make sure we loaded the base configuration correctly
+  ASSERT_EQ(config["root_key"]["option1"]["string_value"].as<std::string>(), "foo_bar");
+
+  const std::vector<bool> expected{true,  true,  false, false, false, true,  true, true,
+                                   false, false, false, false, false, false, false};
+  ASSERT_EQ(config["root_key"]["option1"]["boolean_vector"].as<std::vector<bool>>(), expected);
+  ASSERT_EQ(config["root_key"]["option1"]["floating_scalar"].as<double>(), 10.0);
+  ASSERT_EQ(config["root_key"]["option1"]["integral_scalar"].as<int>(), 1);
+
+  // Feed in another configuration tree as an overlay
   YAML::Node overlay(YAML::LoadFile("trellis/core/test/test_overlay_config.yml"));
-
-  std::cout << "BASE = " << std::endl;
-  std::cout << config.Root() << std::endl;
-
   config.Overlay(overlay);
-  std::cout << "OVERLAY = " << std::endl;
-  std::cout << config.Root() << std::endl;
+
+  // Make sure the overlay sits on top of the base configuration properly
+  ASSERT_EQ(config["root_key"]["option1"]["string_value"].as<std::string>(), "updated_foo_bar");
+
+  const std::vector<bool> expected2{true, true, true};
+  ASSERT_EQ(config["root_key"]["option1"]["boolean_vector"].as<std::vector<bool>>(), expected2);
+  ASSERT_EQ(config["root_key"]["option1"]["floating_scalar"].as<double>(), -4.0);
+  ASSERT_EQ(config["root_key"]["option1"]["integral_scalar"].as<int>(), -100);
+  ASSERT_EQ(config["root_key"]["option1"]["new_value"].as<double>(), 23.0);
 }
